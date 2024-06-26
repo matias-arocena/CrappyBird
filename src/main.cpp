528443a74cd0fb84ded7b65e061e448c0425baa1
Matias Arocena <matias@byte.uy>
2024-06-25 23:25:13 -0300

    Print Hello World in Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include "TiKiRa.h"
+#include "HUD.h"
+#include "TKMath.h"
 
 int main()
 {
-    TiKiRa::Run();
+    TiKiRa::Engine::GetInstance().SetTick([]() {
+        TiKiRa::HUD::GetInstance().DrawText("Hello, World!", 10, 10, 20, {1.0f, 1.0f, 1.0f, 1.0f});
+    });
+    TiKiRa::Engine::GetInstance().Run();
     return 0;
 }
